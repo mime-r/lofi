0e5e3334be24743cda2fe90b7d7664177b9ea414
Nicholas Johansan <nicholasjohansan@gmail.com>
2023-07-23 08:36:50 +0800

    feat: add preferences object

---
+++ js/preferences.js
@@ -0,0 +1,36 @@
+
+const Preferences = (function() {
+  // Stored Preferences
+  /** @var {number} */
+  let volume = 1;
+  /** @var {string} */
+  let theme = "cafe";
+
+  // Methods
+  function loadPreferences() {
+    const preferences = JSON.parse(localStorage.getItem("preferences")) ?? {
+      volume,
+      theme,
+    };
+    volume = preferences.volume;
+    theme = preferences.theme;
+  }
+
+  function savePreferences(newPreferences) {
+    console.log("saving.. ", newPreferences);
+    if (newPreferences.volume !== undefined) volume = newPreferences.volume;
+    if (newPreferences.theme !== undefined) theme = newPreferences.theme;
+    localStorage.setItem("preferences", JSON.stringify({
+      volume: volume,
+      theme: theme
+    }));
+  }
+
+  loadPreferences();
+
+  return {
+    savePreferences,
+    get volume() { return volume },
+    get theme() { return theme; }
+  };
+})();
